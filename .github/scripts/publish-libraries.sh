48d6904cc2365f87b043397c65f7115d51aeae99
Hiroki Gota <hiroki.gota@spatialvision.com.au>
2021-05-03 17:45:51 +1000

    appended a blank space to make up as an array

--- .github/scripts/publish-libraries.sh
+++ .github/scripts/publish-libraries.sh
@@ -32,8 +32,9 @@ echo "GITHUB_BASE_REF: $GITHUB_BASE_REF, NX_BASE: $NX_BASE"
 
 AFFECTED=$(node node_modules/.bin/nx affected:packages --plain --base=$NX_BASE --head=HEAD)
 echo "AFFECTED: '$AFFECTED'"
+
 if [ "$AFFECTED" == "" ]; then
-  AFFECTED="vault-env-config"
+  AFFECTED="vault-env-config "
 fi
 
 if [ "$AFFECTED" != "" ]; then
