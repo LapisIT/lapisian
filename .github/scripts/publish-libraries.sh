a007b5fc0167acee1fb52b9191f976a1454e102b
Hiroki Gota <hiroki.gota@spatialvision.com.au>
2021-05-05 09:26:40 +1000

    try(publish-libraries.sh): removed EOL from the output of affected

--- .github/scripts/publish-libraries.sh
+++ .github/scripts/publish-libraries.sh
@@ -34,7 +34,12 @@ fi
 
 echo "GITHUB_BASE_REF: $GITHUB_BASE_REF, NX_BASE: $NX_BASE"
 AFFECTED=$(node node_modules/.bin/nx affected:libs --plain --base=$NX_BASE --head=HEAD)
+AFFECTED=${AFFECTED//[$'\t\r\n']}
 echo "nx affected:libs --plain --base=$NX_BASE --head=HEAD: '$AFFECTED'"
+
+AFFECTED=$(npm run affected:libs --plain)
+AFFECTED=${AFFECTED//[$'\t\r\n']}
+echo "npm run affected:libs: '$AFFECTED'"
 
 AFFECTED=" vault-env-config "
 echo "Hardcoded AFFECTED: '$AFFECTED'"
