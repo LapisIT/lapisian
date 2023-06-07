23bb9456b849dff62713bf719d9dccbba63fd1ee
Hiroki Gota <hiroki.gota@lapisit.com.au>
2023-06-07 17:07:14 +1000

    feat(nx): Migrated from 12 to 13

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,3 @@
-module.exports = {
-  projects: [, '<rootDir>/packages/vault-env-config'],
-};
+const { getJestProjects } = require('@nrwl/jest');
+
+module.exports = { projects: getJestProjects() };
